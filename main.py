b9671bdcc83df5aa553f92b563e1e7f35cfc631d
rmageddetz <rmagedetz@gmail.com>
2024-08-01 15:49:34 +0600

    commit 0108_3

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ if tg_id:
     st.write(f'Telegram ID: {tg_id}')
 else:
     st.write('Telegram ID not provided.')
-
+st.button("Push")
 # Пример вывода других данных
 st.write('This is your Streamlit app with Telegram integration.')
