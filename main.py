5ef601261ce5a185e40b4703b377dc31697f7a19
rmageddetz <rmagedetz@gmail.com>
2024-08-01 15:24:35 +0600

    commit 0108_3

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
 import streamlit as st
-import json
-import hmac
-import hashlib
 
-# Замените на ваш API токен
-API_TOKEN = st.secrets.tg_bot.token
+# Получение параметров из URL
+query_params = st.experimental_get_query_params()
+tg_id = query_params.get('tg_id', [None])[0]
 
+# Отображение Telegram ID
+st.title('Streamlit App')
+if tg_id:
+    st.write(f'Telegram ID: {tg_id}')
+else:
+    st.write('Telegram ID not provided.')
 
-def verify_telegram_auth_data(auth_data, secret_key):
-    try:
-        auth_data_dict = json.loads(auth_data)
-        data_check_string = '\n'.join(f'{k}={v}' for k, v in sorted(auth_data_dict.items()))
-        hmac_hash = hmac.new(secret_key, data_check_string.encode('utf-8'), hashlib.sha256).hexdigest()
-        return hmac_hash == auth_data_dict.get('hash')
-    except Exception as e:
-        st.error(f"Ошибка верификации: {e}")
-        return False
-
-
-def main():
-    st.title("Ваш Telegram ID")
-
-    # Получите URL-параметры
-    auth_data = st.query_params.get('auth_data', '')
-    st.write(auth_data)
-    if auth_data:
-        secret_key = hashlib.sha256(API_TOKEN.encode('utf-8')).digest()
-        if verify_telegram_auth_data(auth_data, secret_key):
-            auth_data_dict = json.loads(auth_data)
-            user_id = auth_data_dict.get('user', {}).get('id', 'Неизвестно')
-            st.write(f"Ваш Telegram ID: {user_id}")
-        else:
-            st.write("Ошибка получения ID")
-    else:
-        st.write("Ошибка: auth_data не предоставлен")
-
-
-if __name__ == "__main__":
-    main()
+# Пример вывода других данных
+st.write('This is your Streamlit app with Telegram integration.')
